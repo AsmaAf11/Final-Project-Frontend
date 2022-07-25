1fac18c58881844ae859028ffcc750bda49c9ce5
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-25 23:46:50 +0300

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,16 @@
-import React from 'react'
-<<<<<<< HEAD
-
-function Home() {
-  return (
-    <div>Home</div>
-=======
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Cards from "../Categories/Cards";
 
-
-
 function Home() {
   return (
-  <>
-  <Header/>
-  <Cards/>
-  <Footer/>
-
-  </>
->>>>>>> 27de8c1e797d25708f12e184cab22d24908bc4c6
-  )
+    <>
+      <Header />
+      <Cards />
+      <Footer />
+    </>
+  );
 }
 
-export default Home
+export default Home;

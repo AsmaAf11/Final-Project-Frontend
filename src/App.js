0c4c6c2e402cd0aa4e4e7f763dafd4e8388fee09
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-25 17:36:35 +0300

    updated and added article components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import './App.css';
+import "./App.css";
 
-import Header from './components/Home/Header'
+import Header from "./components/Home/Header";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {Container} from "react-bootstrap"
-import Cards from './components/Categories/Cards';
-import Login from './components/registration/Login';
-import Signup from './components/registration/Signup';
-
-
+import { Container } from "react-bootstrap";
+import Cards from "./components/Categories/Cards";
+import Login from "./components/registration/Login";
+import Signup from "./components/registration/Signup";
+import AddArticle from "./components/Article/AddArticle";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-
-    <Header/>
-
-
-<Container>
+      <Container>
         <Router>
           <Routes>
             <Route>
-              <Route path='/categories' element={<Cards/>}></Route>
-              <Route path='/login' element={<Login/>}></Route>
-              <Route path='/signup' element={<Signup/>}></Route>
+              <Route path="/categories" element={<Cards />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/AddArticle" element={<AddArticle />}></Route>
+              <Route path="/Home" element={<Home />}></Route>
             </Route>
           </Routes>
         </Router>
-        </Container>
-      
-
+      </Container>
     </div>
-
   );
 }
 

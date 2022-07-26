450c79a250599acc0ff8094397e4c7a56e3cafd4
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-26 11:55:19 +0300

    added article details

--- src/components/Article/HomeArticles.js
+++ src/components/Article/HomeArticles.js
@@ -8,7 +8,6 @@ function HomeArticles() {
             <div className="box-item col-md-4">
                 <div className="flip-box">
                     <div className="flip-box-front text-center" style={{backgroundImage:`url("https://i.postimg.cc/CLVcBDW8/cta-2.png")`}}>
-
                     </div>
                 </div>
             </div>

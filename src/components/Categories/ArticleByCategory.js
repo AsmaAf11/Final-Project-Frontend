34cc0455515726a7b7994f2327f2b3134bb55890
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-07-26 17:36:05 +0300

    Search Final Edit

--- src/components/Categories/ArticleByCategory.js
+++ src/components/Categories/ArticleByCategory.js
@@ -7,13 +7,10 @@ import like from '../Home/like.png'
 function ArticleByCategory() {
   const { id } = useParams();
   const [data, setData] = useState([]);
-<<<<<<< HEAD
-=======
+
   const navigate = useNavigate();
   
  
-  // const[id, setId] = useState();
->>>>>>> e08cf357046ea63f964a6d76c40a635a340c89bf
 
   useEffect(() => {
     axios
@@ -43,13 +40,29 @@ function ArticleByCategory() {
         console.log(response.data.Article);
          setData(response.data.Article);
 
-<<<<<<< HEAD
+
       })
       .catch((error) => {
         console.log(error.response);
       });
 
   };
+  let [counter, setCounter] = useState(0);
+  const [likes,SetLike]=useState(0)
+
+  const addlike =()=>{
+    setCounter(counter++);
+
+    axios.post ("http://127.0.0.1:8000/add_ArticleLike",{
+        likes
+    }).then(res=>{
+      
+      console.log(res.data);
+      SetLike(res.data.Articles);
+
+        })
+        .catch(err=>{console.log(err)})
+}
   return (
     <>
       <input
@@ -73,58 +86,7 @@ function ArticleByCategory() {
                   <Card.Text className="cardParagraphText">
                     {e.summary}
                   </Card.Text>
-
-                  {/* <div className="likeborder">
-                <Card.Text  ><img className="cardlikeimg" src={like}/> {e.likes}</Card.Text>
-                </div> */}
-                  <Card.Footer className="text-muted">
-                    Publisehd at: {e.created_at} by {e.publisher}
-                  </Card.Footer>
-                </Card.Body>
-              </Card>
-            </Container>
-          );
-        })}
-      </div>
-    </>
-=======
-
-
-
-  let [counter, setCounter] = useState(0);
-  const [likes,SetLike]=useState(0)
-
-  const addlike =()=>{
-    setCounter(counter++);
-
-    axios.post ("http://127.0.0.1:8000/add_ArticleLike",{
-        likes
-    }).then(res=>{
-      
-      console.log(res.data);
-      SetLike(res.data.Articles);
-
-        })
-        .catch(err=>{console.log(err)})
-}
-  return (
-    <>
-    <input type="" className="search-box" placeholder="Search" onChange={SearchHandle}/>
-    <div className="cardsRow">
-      {data.map((e) => {
-        return (
-          <Container className="pt-5">
-            <Card style={{ width: "30rem" }}>
-              <Card.Img
-                style={{ width: "100%", height: "350px" }}
-                variant="top"
-                src={e.image}
-              />
-              <Card.Body>
-                <Card.Title className="cardTitleText">{e.title}</Card.Title>
-                <Card.Text className="cardParagraphText">{e.summary}</Card.Text>
-                
-                <span className="d-flex">
+                  <span className="d-flex">
                 <div className="likeborder">
                 <Card.Text> <div className="clickonimg" onClick={addlike}><img className="cardlikeimg" src={like}/></div> {e.likes}</Card.Text>
                 </div>
@@ -137,16 +99,17 @@ function ArticleByCategory() {
                 >
                   View Articles
                 </Button>
-                <Card.Footer className="text-muted">
-                  Publisehd at: {e.created_at} by {e.publisher}
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-          </Container>
-        );
-      })}
-    </div></>
->>>>>>> e08cf357046ea63f964a6d76c40a635a340c89bf
-  );
+                  <Card.Footer className="text-muted">
+                    Publisehd at: {e.created_at} by {e.publisher}
+                  </Card.Footer>
+                </Card.Body>
+              </Card>
+            </Container>
+          );
+        })}
+      </div>
+    </>
+  
+  )
 }
 export default ArticleByCategory;

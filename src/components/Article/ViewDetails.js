bfbf8abb787ca1638cedd4429e7794d546ff464d
Asma Abufayah <aaha44@gmail.com>
2022-07-26 16:00:48 +0300

    m

--- src/components/Article/ViewDetails.js
+++ src/components/Article/ViewDetails.js
@@ -8,7 +8,6 @@ import { useParams } from "react-router-dom";
 export default function ViewDetails() {
     const { id } = useParams();
     const [data, setData] = useState([]);
-    const [comment,setComment]= useState([])
   
     useEffect(() => {
       axios
@@ -27,7 +26,7 @@ export default function ViewDetails() {
         .get(`http://127.0.0.1:8000/view_comment/${id}/`)
         .then((res) => {
           console.log(res.data);
-          setComment(res.data.application);
+          setData(res.data.Comment);
         })
         .catch((err) => {
           console.log(err);
@@ -61,13 +60,14 @@ export default function ViewDetails() {
 
         );
       })}
-            {comment.map((ec) => {
+            {data.map((ec) => {
         return (
           <Container className="pt-5">
             <Card>
               <Card.Body>
                 <Card.Title className="cardTitleText">Username: {ec.user}</Card.Title>
                 <Card.Text className="cardParagraphText">Comment:<br /> {ec.content}</Card.Text>
+                <Card.Text className="cardParagraphText">Summary:<br />  {ec.summary}</Card.Text>
   
                 <Card.Footer className="text-muted">
                   Commented at: {ec.created_at}

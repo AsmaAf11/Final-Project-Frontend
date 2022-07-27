9457bbb2c64b760e68a343ab67e6123c07c7d6d7
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-28 01:25:34 +0300

    update bookmark style

--- src/components/BookMark/FavCategory.js
+++ src/components/BookMark/FavCategory.js
@@ -1,86 +1,147 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import PArticles from '../Article/PArticles'
+import PArticles from "../Article/PArticles";
 import { Card, Container, Button } from "react-bootstrap";
 
 function FavCategory() {
-    const { id } = useParams();
-    const [data, setData] = useState([]);
-    const [deleteFavCategory, setDeleteFavCategory] = useState([]);
-    const navigate = useNavigate();
-    const accessToken = localStorage.getItem("token");
-  
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [deleteFavCategory, setDeleteFavCategory] = useState([]);
+
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  };
+
+  useEffect(() => {
     const config = {
       headers: { Authorization: `Bearer ${accessToken}` },
     };
-    
-    useEffect(() => {
-      const config = {
-        headers: { Authorization: `Bearer ${accessToken}` }}
-      axios
-        .get(`http://127.0.0.1:8000/list_favCategory/`, config)
-        .then((res) => {
-          console.log(res.data.Favouites);
-          setData(res.data.Favouites);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, []);
-  
-  
-    const deleteFav= () => {
-      axios
-        .delete(`http://127.0.0.1:8000/delete_favCategory/${deleteFavCategory}/`, config)
-        .then((res) => {
-          setData(res.data.Favouites);
-          alert('deleted')
-          window.location.reload();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-  
-  
-    return (
+    axios
+      .get(`http://127.0.0.1:8000/list_favCategory/`, config)
+      .then((res) => {
+        console.log(res.data.Favouites);
+        setData(res.data.Favouites);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-        <div >
-      {data.map((e) => {
-        return (
-          <Container className="pt-5">
-            
-              <Card style={{ width: "30rem" }} className="border-dark">
-              <div
-              onClick={() => {
-                navigate(`/articlesCategories/${e.category.id}`);
-              }}
-            >   <Card.Img
+  const deleteFav = () => {
+    axios
+      .delete(
+        `http://127.0.0.1:8000/delete_favCategory/${deleteFavCategory}/`,
+        config
+      )
+      .then((res) => {
+        setData(res.data.Favouites);
+        alert("deleted");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <h4 style={{ margin: "100px 0 20px 80px",color: "#545454" }}>I'm Interested in ..</h4>
+      <div
+        className="row"
+        style={{
+          width: "100%",
+          height: "100%",
+          textAlign: "center",
+          position: "relative",
+          margin: "0 auto",
+        }}
+      >
+        {data.map((e) => {
+          return (
+            <div className="col-6 col-md-4" style={{marginLeft:'70px'}}
+            >
+              {/* <Card style={{width:'100%',height:'100px',display:'inline-block',lineHeight:'100px',backgroundColor:'white',marginBottom:'30px'}}className="border-dark"> */}
+              <Card
+                style={{ marginBottom: "20px" , justifyContent:'space-around'}}
+                className="border-dark"
+              >
+                <div
+                  onClick={() => {
+                    navigate(`/articlesCategories/${e.category.id}`);
+                  }}
+                >
+                  <Card.Img
+                    style={{ width: "100%", height: "200px" }}
+                    variant="top"
+                    src={e.category.image}
+                  />
+                </div>
+                <Card.Body style={{ width: "100%", height: "100px" }}>
+                  <Card.Title>
+                    <h5 style={{textTransform:'capitalize'}}>{e.category.name}</h5>
+                  </Card.Title>
+                  <Card.Text>
+                    <Button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        setDeleteFavCategory(e.id);
+                        deleteFav();
+                      }}
+                    >
+                      Unfavourite
+                    </Button>{" "}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default FavCategory;
+
+{
+  /* <div>
+{data.map((e) => {
+  return (
+    <Container className="pt-5">
+      <Card style={{ width: "30rem" }} className="border-dark">
+        <div
+          onClick={() => {
+            navigate(`/articlesCategories/${e.category.id}`);
+          }}
+        >
+          {" "}
+          <Card.Img
             style={{ width: "100%", height: "350px" }}
             variant="top"
             src={e.category.image}
-          /></div>
-                <Card.Body>
-                  <Card.Title className="cardTitleText">{e.category.name}</Card.Title>
-                  
-                </Card.Body>
-                <Button className="btn btn-danger"
-                    onClick={()=>{
-                      setDeleteFavCategory(e.id);
-                      deleteFav();
-                    }}
-                    style={{ marginBottom:'20px'}}
-                  >
-                    Unfavourite
-                  </Button>
-              </Card>
-          </Container>
-        );
-      })}
-    </div>
-
-    )
-  }
-
-export default FavCategory
+          />
+        </div>
+        <Card.Body>
+          <Card.Title className="cardTitleText">
+            {e.category.name}
+          </Card.Title>
+        </Card.Body>
+        <Button
+          className="btn btn-danger"
+          onClick={() => {
+            setDeleteFavCategory(e.id);
+            deleteFav();
+          }}
+          style={{ marginBottom: "20px" }}
+        >
+          Unfavourite
+        </Button>
+      </Card>
+    </Container>
+  );
+})}
+</div> */
+}
--- src/components/Categories/Cards.js
+++ src/components/Categories/Cards.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Card, Container, Button, Row } from "react-bootstrap";
-import bookmark from './bookmark.png'
+import bookmark from "./bookmark.png";
 function Cards() {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
@@ -25,13 +25,12 @@ function Cards() {
       });
   }, []);
 
-
   const addFavCategory = () => {
     axios
       .post(`http://127.0.0.1:8000/add_favCategory/${idCategory}/`, {}, config)
       .then((res) => {
         console.log(res.data);
-        alert('added')
+        alert("added");
         // window.location.reload();
       })
       .catch((error) => {
@@ -40,51 +39,65 @@ function Cards() {
   };
 
   return (
+    <>
+      <div style={{ marginTop: "100px" }}></div>
 
-
-<>
-
-
-
-
-    <div className="row " style={{width:'80%',height:'100%',textAlign:'center',position:'relative',margin:'0 auto'}}>
-      {data.map((e) => {
-        return (
-          <div className="col-8 col-md-6">
-            
-            {/* <Card style={{width:'100%',height:'100px',display:'inline-block',lineHeight:'100px',backgroundColor:'white',marginBottom:'30px'}}className="border-dark"> */}
-              <Card style={{ width: "28rem" ,  marginBottom:'20px'}} className="border-dark">
-              <div className="categoryimg clickonimg"
-              onClick={() => {
-                navigate(`/articlesCategories/${e.id}`);
-              }}
-            >
-                <Card.Img
-                  style={{ width: "100%", height: "200px" }}
-                  variant="top"
-                  src={e.image}
-                />
+      <div
+        className="row "
+        style={{
+          width: "80%",
+          height: "100%",
+          textAlign: "center",
+          position: "relative",
+          margin: "0 auto",
+        }}
+      >
+        {data.map((e) => {
+          return (
+            <div className="col-8 col-md-6">
+              {/* <Card style={{width:'100%',height:'100px',display:'inline-block',lineHeight:'100px',backgroundColor:'white',marginBottom:'30px'}}className="border-dark"> */}
+              <Card
+                style={{ width: "28rem", marginBottom: "20px" }}
+                className="border-dark"
+              >
+                <div
+                  className="categoryimg clickonimg"
+                  onClick={() => {
+                    navigate(`/articlesCategories/${e.id}`);
+                  }}
+                >
+                  <Card.Img
+                    style={{ width: "100%", height: "200px" }}
+                    variant="top"
+                    src={e.image}
+                  />
                 </div>
-                <Card.Body
-               style={{ width: "100%", height: "1px" }}>
-                  <Card.Title  className="cardTitle" >{e.name}</Card.Title>
+                <Card.Body style={{ width: "100%", height: "1px" }}>
+                  <Card.Title style={{textTransform:'capitalize'}}>{e.name}</Card.Title>
                 </Card.Body>
-                
-                <div className="d-flex" onClick={()=>{
-                  setIdCategory(e.id);
-                  addFavCategory();
-                  alert('added')
-                }} ><img  className="clickonimg" style={{  height: "40px" ,backgroundColor:"white"}} src={bookmark}/></div>
 
-               
+                <div
+                  className="d-flex"
+                  onClick={() => {
+                    setIdCategory(e.id);
+                    addFavCategory();
+                    alert("added");
+                  }}
+                >
+                  <img
+                    className="clickonimg"
+                    style={{ height: "40px", backgroundColor: "white" }}
+                    src={bookmark}
+                    alt=' '
+                  />
+                </div>
               </Card>
-          </div>
-        );
-      })}
-    </div>
-      </>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
 export default Cards;
-
